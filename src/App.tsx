c7ef30e4b3926ea6dd7f0b7c6e3b4c6d4e223e65
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-07-28 15:35:07 +0530

    add navigation and dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import AuthPage from "./pages/AuthPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <AuthPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
